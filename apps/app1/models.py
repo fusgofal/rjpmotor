1d8f33daa77e83ed0ed2205ef05f089590137d0c
fusgofal <fusgofal@gmail.com>
2016-11-22 08:23:11 -0500

    entrega4

--- apps/app1/models.py
+++ apps/app1/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Category(models.Model):
+	name = models.CharField(max_length=50)
+	def __unicode__(self):
+		return self.name
+class City(models.Model):
+	name = models.CharField(max_length=50)
+	def __unicode__(self):
+		return self.name
+
+class Payment(models.Model):
+	pay = models.CharField(max_length=50)
+	def __unicode__(self):
+		return self.pay
+
+class Restaurant(models.Model):
+	payment = models.ManyToManyField(Payment)
+	category = models.ManyToManyField(Category)
+	name = models.CharField(max_length=50)
+	desciption = models.CharField(max_length=200)
+	image = models.ImageField(upload_to = 'fotos')
+
+	def __unicode__(self):
+		return self.name
+
+class Establishment(models.Model):
+	restaurant = models.ForeignKey(Restaurant)
+	city = models.ForeignKey(City)
+	address = models.CharField(max_length=50)
+
+	def __unicode__(self):
+		return self.address
+
+class Tip(models.Model):
+	restaurant = models.ForeignKey(Restaurant)
+	user = models.ForeignKey(User)
+	content = models.CharField(max_length=200)
